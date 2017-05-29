a0bce5b4b87879b13f177a1dcabf2b2645e6aafc
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2017-05-29 23:17:26 +0200

    Gather attributes data function working.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 
 #include "textDataReader.h"
 
@@ -10,6 +11,10 @@ struct attributeData
   std::string attributeType;
 };
 
+std::vector<attributeData> attributes;
+
+void gatherAttributesData(textDataReader* tdr);
+
 int main()
 {
   std::ifstream sourceFile("D:\\Dysk Google\\Data Streams\\powersupply.arff");
@@ -18,11 +23,46 @@ int main()
 
   textDataReader tdr(&sourceFile);
 
-  tdr.getNextRawDatum(&line);
-  tdr.getNextRawDatum(&line);
+  gatherAttributesData(&tdr);
 
-
-  std::cout << line;
+  for(int i = 0; i < attributes.size(); ++i)
+  {
+    std::cout << attributes.at(i).attributeName << " ";
+    std::cout << attributes.at(i).attributeType << std::endl;
+  }
 
   return 0;
+}
+
+void gatherAttributesData(textDataReader* tdr)
+{
+  // This only works for .arff files. Not tested for others.
+
+  std::string line;
+
+  // While line doesn't start with @attribute
+  while(line.find("@attribute")) tdr->getNextRawDatum(&line);
+
+  // While line starts with @attribute
+  while(!line.find("@attribute"))
+  {
+    std::istringstream ss(line);
+    std::string substring;
+
+    getline(ss, substring, ' ');
+
+    attributes.push_back(attributeData());
+
+    getline(ss, substring, ' ');
+
+    attributes.at(attributes.size()-1).attributeName = substring;
+
+    getline(ss, substring, ' ');
+
+    if(substring.find("Numeric")==0) attributes.at(attributes.size()-1).attributeType = substring;
+    else attributes.at(attributes.size()-1).attributeType = "Symbolic";
+
+    tdr->getNextRawDatum(&line);
+  }
+
 }
