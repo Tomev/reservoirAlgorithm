225659d22019d09f7b4ac6311191622371bd2cba
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2017-06-05 19:06:01 +0200

    Removed .idea and cmake folders (hopefully). Moved sample struct to data parser.

--- dataParser.h
+++ dataParser.h
@@ -5,6 +5,27 @@
 #ifndef RESERVOIRALGORITHM_DATAPARSER_H
 #define RESERVOIRALGORITHM_DATAPARSER_H
 
+#include <string>
+#include <vector>
+#include <c++/iostream>
+
+struct sample
+{
+  long dataId;
+  std::vector<std::string> values;
+
+  void print()
+  {
+    std::cout << "Step: " << dataId << ". Data: ";
+
+    for(int i = 0; i < values.size(); ++i)
+    {
+      std::cout << values.at(i) << ", ";
+    }
+
+    std::cout << std::endl;
+  }
+};
 
 class dataParser
 {
